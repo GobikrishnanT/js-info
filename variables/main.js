e19d162860ff1356af7425dd69d4408f968eff30
Gobikrishnan <gobikrishnantj@gmail.com>
2023-10-18 07:56:37 +0530

    Js info learning - variables, its storage and its assignment

---
+++ variables/main.js
@@ -0,0 +1,15 @@
+// Task 1 : 
+
+const name = "john";
+console.log(name);
+const admin = name ;
+alert(admin);
+
+
+// Task 2 : 
+const jupiter = "jupiter";
+const userName = "Gobi";
+
+// Case sensitive : 
+const APPLE = 'APPLE'; 
+const apple = 'apple';
